4581bee385454bb3fee514727875df905d484c41
56592aya <yazdiha@ese.eur.nl>
2017-03-30 15:51:45 +0200

    relatively optimized the utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -48,6 +48,7 @@ function logsumexp{T<:Real}(x::T, y::T)                       #dispatch #1
     x == y && abs(x) == Inf && return x
     x > y ? x + log1p(exp(y - x)) : y + log1p(exp(x - y))
 end
+
 #############################################################
 function logsumexp{T<:Real}(x::AbstractArray{T})              #dispatch #2
     S = typeof(exp(zero(T)))    # because of 0.4.0
@@ -65,40 +66,45 @@ end
 # matrices based on the values in each row so that, rows
 # with higher values in similar columns are located closer
 # to each other.
-function sort_by_argmax!(mat::Array{Float64, 2})
-    n_row=size(mat)[1]
-    ind_max=zeros(Int64, n_row)
-    for a in 1:n_row
-        ind_max[a] = indmax(mat[a,:])
-    end
-    mat_tmp = similar(mat)
-    count = 1
-    for j in 1:maximum(ind_max)
-        for i in 1:n_row
-            if ind_max[i] == j
-                mat_tmp[count,:] = mat[i,:]
-                count = count + 1
-            end
+function sort_by_argmax!{T<:Real}(mat::AbstractArray{T,2})
+  n_row=size(mat,1)
+  n_col = size(mat,2)
+  ind_max=zeros(Int64, n_row)
+  @simd for a in 1:n_row
+      @inbounds ind_max[a] = indmax(view(mat,a,1:n_col))
+  end
+  mat_tmp = similar(mat)
+  count = 1
+  for j in 1:maximum(ind_max)
+    for i in 1:n_row
+      if ind_max[i] == j
+        for k in 1:n_col
+          mat_tmp[count,k] = mat[i,k]
         end
+        count += 1
+      end
     end
-    # This way of assignment is important in arrays, el by el
-    mat[:]=mat_tmp[:]
-    mat
+  end
+  # This way of assignment is important in arrays, el by el
+  mat[:]=mat_tmp[:]
+  mat
 end
+x = rand(10000,100)
 ###############################################################
 ###############################################################
 #####################  EXPORTING  #############################
 ###############################################################
 ###############################################################
-function row_normalize(arr::Array{Float64,1})
-  s = 0.0
-  for k in 1:length(arr)
+
+function row_normalize{T<:Real}(arr::AbstractArray{T})
+  s = zero(eltype(arr))
+  @fastmath @inbounds @simd for k in 1:length(arr)
     s += arr[k]
   end
-  for k in 1:length(arr)
+  @fastmath @inbounds @simd for k in 1:length(arr)
     arr[k] /= s
   end
-  return arr
+  arr
 end
 export Node, Link,NonLink, logsumexp, sort_by_argmax!, row_normalize
 
